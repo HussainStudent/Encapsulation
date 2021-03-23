45693ed6a0ef818896c6b8c955c985a14c7da0a5
lenovo <lenovo@192.168.1.80>
2021-03-23 08:09:55 +0300

    this is my Encapsulation code

---
+++ Encapsulation/src/HussainAge.java
@@ -0,0 +1,10 @@
+
+public class HussainAge {
+	public static void main(String[] atgs){
+	MyAge hussain = new MyAge();
+	hussain.setAge(21);
+	System.out.println("this is my age : "+ hussain.getAge());
+	}
+	
+	
+}
---
+++ Encapsulation/src/MyAge.java
@@ -0,0 +1,12 @@
+
+public class MyAge {
+	private int Age;
+//Getter
+	public int getAge(){
+		return Age;
+	}
+	//seter
+	void  setAge(int newAge){
+		Age = newAge;
+	}
+}
